754bd7dc12e6c787998b7161e58b9a989f5b53a6
w <wangox@gmail.com>
2015-04-23 06:22:42 +0800

    first commit

---
+++ models/deposit.go
@@ -0,0 +1,20 @@
+//
+//
+//
+
+package models
+
+// 处理存款有关的功能
+
+type Deposit struct {
+}
+
+// 提交提款申请
+func (d *Deposit) CommitRequst() error {
+
+}
+
+// 查询
+func (d *Deposit) QueryRequest() error {
+
+}
